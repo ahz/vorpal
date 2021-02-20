a9bb89fe81b7682d505414f129ba688aa0ab4452
Andrej Hazucha <andrej.hazucha@infor.com>
2021-02-20 09:56:50 +0100

    Updated to Node.js >= 12
      - removed dist and pointing main to lib
      - removed babel and build
      - updated gulp and plugins
      - added package-lock.json

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,3 @@ gulp.task('lint', () => {
   return gulp.src(paths.src)
     .pipe(_gulp.xo());
 });
-
-gulp.task('build', () => {
-  return gulp.src(paths.src)
-    .pipe(_gulp.babel())
-    .pipe(gulp.dest(paths.dist));
-});
-
-gulp.task('watch', ['build'], () => {
-  gulp.watch(paths.src, ['build']);
-});
-
-gulp.task('default', ['watch']);
